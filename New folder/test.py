8bf5d43f80c244b38ccfc1cc337c36d37ee0fee6
Balaji-Coder06 <sbalaji272006@gmail.com>
2025-04-12 11:18:39 +0530

    New Edit

--- New folder/test.py
+++ New folder/test.py
@@ -20,7 +20,7 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 spoken = ""
 last_spoken_time = time.time()
